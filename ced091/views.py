3959694f9faa9612e5b76d1e1d37eedb574a0372
ced091 <ced091@hotmail.fr>
2023-03-06 19:00:59 +0100

    modification de la page main

--- ced091/views.py
+++ ced091/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def main(request):
-    return render(request, "ced091/main.html")
+    date_now = datetime.now()
+    context = {
+        "date_nom": str(date_now)
+    }
+    return render(request, "ced091/main.html", context)
