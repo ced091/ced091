4be35dc4cbb32efd01f5b200133f829299a872f4
ced091 <ced091@hotmail.fr>
2023-03-06 18:48:45 +0100

    ajout d'une vue main et d'un lien vers la station météo

--- ced091/urls.py
+++ ced091/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('station_meteo.urls')),
+    path('', views.main, name="main"),
+    path('station', include('station_meteo.urls')),
     path('admin/', admin.site.urls),
 ]
