81ec6a248268d1758bbc1f1c3f21a26741d8ed7e
ced 091 <ced091@hotmail.fr>
2023-03-22 18:55:42 +0100

    modif non sus

--- station_meteo/urls.py
+++ station_meteo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
+from . import ajax
 
 app_name = 'station_meteo'
 
 urlpatterns = [
     path('', views.accueil, name='accueil'),
-
     path('add_bme280/', views.add_bme280, name='add_bme280'),
+    path('refresh_data/', ajax.refresh_data, name="refresh_data"),
 ]
