34f11ab6ff09dd939e5d2f463c8699da6b689243
ced091 <ced091@hotmail.fr>
2023-03-26 11:29:42 +0200

    changement du format de la date accueil

--- ced091/views.py
+++ ced091/views.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 def main(request):
     date_now = datetime.now()
+    affichage_date = date_now.strftime("%d-%m-%Y %H:%M:%S")
     context = {
-        "date_nom": str(date_now)
+        "date_nom": affichage_date
     }
     return render(request, "ced091/main.html", context)
