18c0a70de607b5bc18ad9c9f51489a9c1ceadcc5
Cédric Villebasse <cedric.villebasse@universite-paris-saclay.fr>
2024-01-28 16:01:08 +0100

    add contact lignee numerologie

--- libeiki/urls.py
+++ libeiki/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('quisuisje/', views.quisuisje, name='quisuisje'),
     path('massage-visage/', views.visage, name='visage'),
     path('sophrologie/', views.sophrologie, name='sophrologie'),
+    path('numerologie/', views.numerologie, name='numerologie'),
+    path('lignee-reiki/', views.lignee_reiki, name="lignee_reiki"),
+    path('contact/', views.contact, name='contact'),
 ]
--- libeiki/views.py
+++ libeiki/views.py
@@ -6,6 +6,9 @@ def accueil(request):
 def reiki(request):
     return render(request, "libeiki/reiki.html")
 
+def numerologie(request):
+    return render(request, "libeiki/numerologie.html")
+
 def quisuisje(request):
     return render(request, "libeiki/quisuisje.html")
 
@@ -14,3 +17,9 @@ def visage(request):
 
 def sophrologie(request):
     return render(request, "libeiki/sophrologie.html")
+
+def lignee_reiki(request):
+    return render(request, "libeiki/lignee-reiki.html")
+
+def contact(request):
+    return render(request, "libeiki/contact.html")
