f0a29fba413f762578085cbcb684c7d3e0b435af
Cédric Villebasse <cedric.villebasse@universite-paris-saclay.fr>
2024-02-03 09:25:47 +0100

    change something on settings

--- ced091/settings.py
+++ ced091/settings.py
@@ -146,3 +146,5 @@ STATIC_ROOT = Path(__file__).resolve().parent.parent.parent
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
