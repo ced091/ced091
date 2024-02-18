fb7763d3e1d6df8abf5aae4fa916400aa00baff8
Cédric Villebasse <cedric.villebasse@universite-paris-saclay.fr>
2024-02-18 17:43:00 +0100

    add log everywhere

--- ced091/views.py
+++ ced091/views.py
@@ -1,7 +1,28 @@
+import logging
+
 from django.shortcuts import render
 from datetime import datetime
 
+from django.utils import timezone
+
+from datetime import datetime, timedelta
+
+logger = logging.getLogger(__name__)
+
+def log_user(request, vue):
+    username = request.user.username if request.user.is_authenticated else 'Anonymous'
+    current_datetime = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    if ip_address is None:
+        # Si l'en-tête X-Forwarded-For n'est pas disponible, utilisez l'adresse IP par défaut
+        ip_address = request.META.get('REMOTE_ADDR', 'unknown')
+    else:
+        # Séparez les adresses IP si X-Forwarded-For contient plusieurs adresses
+        ip_address = ip_address.split(",")[0].strip()
+    logger.info(f'[{current_datetime}][{username}][{ip_address}] Accès à la vue {vue}')
+
 def main(request):
+    log_user(request, "main")
     date_now = datetime.now()
     affichage_date = date_now.strftime("%d-%m-%Y %H:%M:%S")
     context = {
--- libeiki/views.py
+++ libeiki/views.py
@@ -1,10 +1,27 @@
+import logging
+
 from django.shortcuts import render, redirect
 from .forms import CommentaireForm
 from django.core.mail import EmailMessage
+from django.utils import timezone
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Commentaire
+
+logger = logging.getLogger(__name__)
+
+def log_user(request, vue):
+    username = request.user.username if request.user.is_authenticated else 'Anonymous'
+    current_datetime = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    if ip_address is None:
+        # Si l'en-tête X-Forwarded-For n'est pas disponible, utilisez l'adresse IP par défaut
+        ip_address = request.META.get('REMOTE_ADDR', 'unknown')
+    else:
+        # Séparez les adresses IP si X-Forwarded-For contient plusieurs adresses
+        ip_address = ip_address.split(",")[0].strip()
+    logger.info(f'[{current_datetime}][{username}][{ip_address}] Accès à la vue {vue}')
 
 # def envoyer_email_apres_soumission(commentaire, **kwargs):
 #     message = f"Un nouveau commentaire a été soumis le {commentaire.date_com}.\n Voici son contenu : {commentaire.texte}. \n Sa note  : {commentaire.note}. \n Le pseudo : {commentaire.pseudo} \n Vous pouvez le rendre visible sur le site en cochant la case 'visible' dans le mode admin de votre site/admin.\n Tchuss."
@@ -17,6 +34,7 @@ from .models import Commentaire
 #     email_msg.send()
 
 def accueil(request):
+    log_user(request, "accueil/libeiki")
     commentaires = Commentaire.objects.filter(visible=True)
 
     # Définissez le nombre d'éléments par page
@@ -42,36 +60,47 @@ def accueil(request):
     return render(request, "libeiki/accueil.html", context)
 
 def reiki(request):
+    log_user(request, "reiki")
     return render(request, "libeiki/reiki.html")
 
 def numerologie(request):
+    log_user(request, "numerologie")
     return render(request, "libeiki/numerologie.html")
 
 def quisuisje(request):
+    log_user(request, "quisuisje")
     return render(request, "libeiki/quisuisje.html")
 
 def massage(request):
+    log_user(request, "massage")
     return render(request, "libeiki/massage.html")
 
 def sophrologie(request):
+    log_user(request, "sophrologie")
     return render(request, "libeiki/sophrologie.html")
 
 def lignee_reiki(request):
+    log_user(request, "lignee_reiki")
     return render(request, "libeiki/lignee-reiki.html")
 
 def contact(request):
+    log_user(request, "contact")
     return render(request, "libeiki/contact.html")
 
 def tarif_reiki(request):
+    log_user(request, "tarif_reiki")
     return render(request, "libeiki/tarif-reiki.html")
 
 def tarif_numerologie(request):
+    log_user(request, "tarif_numerologie")
     return render(request, "libeiki/tarif-numerologie.html")
 
 def tarif_massage(request):
+    log_user(request, "tarif_massage")
     return render(request, "libeiki/tarif-massage.html")
 
 def tarif_sophrologie(request):
+    log_user(request, "tarif_sophrologie")
     return render(request, "libeiki/tarif-sophrologie.html")
 
 def generer_star_rating(value):
@@ -87,7 +116,7 @@ def generer_star_rating(value):
 
 def send_commentaire(request):
     
-    print(request.POST)
+    log_user(request, "send_commentaire")
     if request.POST:
         form = CommentaireForm(request.POST)
         if form.is_valid():
